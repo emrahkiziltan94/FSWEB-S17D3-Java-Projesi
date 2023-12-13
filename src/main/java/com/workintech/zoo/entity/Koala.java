9309e69d853552a35a5d073e6a6dec9ea6fab8f2
mrhkzltn <emrahkiziltan94@gmail.com>
2023-12-13 18:29:23 +0300

    s17d3 solution

---
+++ src/main/java/com/workintech/zoo/entity/Koala.java
@@ -0,0 +1,15 @@
+package com.workintech.zoo.entity;
+
+import lombok.Data;
+
+@Data
+public class Koala {
+    private Integer id;
+
+    private String name;
+    private Double sleepHour;
+
+    private Double weight;
+
+    private String gender;
+}
